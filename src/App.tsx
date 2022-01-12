31d37ec5c72d95b9271321ae2bb790a9609801c8
Alejandro Requejo <arequef@gmail.com>
2022-01-11 18:07:51 -0700

    add initial board with letters selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import React from 'react';
+import { Board } from './components';
+import { Character } from './types';
+import { initBoard } from './utils/board';
+
 export default function App() {
-  return <h1 className="text-2xl">Basta</h1>;
+  const [board, setBoard] = React.useState(() => initBoard());
+
+  const handleToggleEnabled = React.useCallback((character: Character) => {
+    setBoard((prevBoard) =>
+      prevBoard.map((letter) =>
+        letter.character === character
+          ? { ...letter, isEnabled: !letter.isEnabled }
+          : letter
+      )
+    );
+  }, []);
+
+  return (
+    <>
+      <Board board={board} onToggleEnabled={handleToggleEnabled} />
+
+      <div className="flex justify-end mt-4">
+        <button
+          className="px-8 py-2 bg-blue-500 text-white rounded shadow"
+          onClick={() => setBoard(initBoard())}
+        >
+          Reset
+        </button>
+      </div>
+    </>
+  );
 }
