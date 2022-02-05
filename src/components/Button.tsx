6acf4603f75223c40211775fb8e656cb1d5f921a
Alejandro Requejo <arequef@gmail.com>
2022-02-05 12:37:53 -0700

    refactor button component, board letters, add custom tailwind plugins

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import clsx from 'clsx';
 
-const Button = React.forwardRef<
-  HTMLButtonElement,
-  React.ButtonHTMLAttributes<HTMLButtonElement>
->(({ className = '', ...props }, forwardedRef) => (
-  <button
-    className={clsx(
-      'rounded bg-blue-500 px-8 py-2 text-white shadow disabled:cursor-not-allowed disabled:opacity-70',
-      className
-    )}
-    {...props}
-    ref={forwardedRef}
-  />
-));
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'normal' | 'letter';
+};
 
-Button.displayName = 'Button';
+/**
+ * Pushable button based on Josh W Comeau's 3D MVP button.
+ * @see https://www.joshwcomeau.com/animation/3d-button/#our-strategy
+ */
+export default React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { variant = 'normal', children, ...props },
+  forwardedRef
+) {
+  const isLetterVariant = variant === 'letter';
 
-export default Button;
+  return (
+    <button
+      ref={forwardedRef}
+      className={clsx({
+        'group cursor-pointer rounded-lg border-0 bg-blue-800 p-0 outline-offset-4 transition-transform disabled:cursor-not-allowed disabled:opacity-70 pressed:bg-gray-300 current:scale-110 current:bg-green-800 past:bg-blue-300':
+          true,
+        'disabled:opacity-100': isLetterVariant,
+      })}
+      {...props}
+    >
+      <span
+        className={clsx({
+          'block -translate-y-[6px] rounded-lg bg-blue-600 px-8 py-2 text-lg text-white group-active:-translate-y-[2px]':
+            true,
+          'px-2 uppercase group-pressed:-translate-y-[2px] group-pressed:bg-gray-100 group-pressed:text-gray-600 group-current:bg-green-600 group-past:-translate-y-[2px] group-past:bg-blue-100 group-past:text-blue-900':
+            isLetterVariant,
+        })}
+      >
+        {children}
+      </span>
+    </button>
+  );
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('pressed', 'button[aria-pressed="true"]');
+      addVariant('group-pressed', ':merge(button[aria-pressed="true"]) &');
+      addVariant('current', 'button[data-current="true"]');
+      addVariant('group-current', ':merge(button[data-current="true"]) &');
+      addVariant('past', 'button[data-past="true"]');
+      addVariant('group-past', ':merge(button[data-past="true"]) &');
+    }),
+  ],
 };
