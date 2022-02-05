fb7f26d2d2ea3a263b9e189fa3b65319ed351bfd
Alejandro Requejo <16929242+arequejo@users.noreply.github.com>
2022-02-05 13:06:01 -0700

    improve board ui, update tests

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import { Button } from '.';
 import { Character, Letter } from '../types';
 
@@ -16,31 +15,20 @@ export default function Board({
   onToggleEnabled,
 }: BoardProps) {
   return (
-    <main
-      className="grid grid-cols-2 gap-4 rounded bg-white p-4 shadow"
-      data-testid="board"
-    >
-      <span
-        className="self-center justify-self-center text-9xl uppercase"
-        data-testid="current-character"
-      >
-        {current ?? '--'}
-      </span>
-      <div className="grid grid-cols-5 gap-4">
-        {board.map(({ character, isEnabled, hasBeenPicked }) => (
-          <Button
-            key={character}
-            variant="letter"
-            data-current={character === current}
-            data-past={hasBeenPicked && character !== current}
-            aria-pressed={!isEnabled}
-            disabled={isPlaying}
-            onClick={() => onToggleEnabled(character)}
-          >
-            {character}
-          </Button>
-        ))}
-      </div>
+    <main className="grid grid-cols-5 gap-6" data-testid="board">
+      {board.map(({ character, isEnabled, hasBeenPicked }) => (
+        <Button
+          key={character}
+          variant="letter"
+          data-current={character === current}
+          data-past={hasBeenPicked && character !== current}
+          aria-pressed={!isEnabled}
+          disabled={isPlaying}
+          onClick={() => onToggleEnabled(character)}
+        >
+          {character}
+        </Button>
+      ))}
     </main>
   );
 }
