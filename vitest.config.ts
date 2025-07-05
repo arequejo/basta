8d0f418d8a407d6197ddbd6260331b681c8fe98a
Alejandro Requejo <16929242+arequejo@users.noreply.github.com>
2025-07-05 14:39:57 -0600

    cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,12 +4,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: './src/test/setup.ts',
     browser: {
       enabled: true,
       provider: 'playwright',
       // https://vitest.dev/guide/browser/playwright
-      instances: [{ browser: 'chromium' }],
+      instances: [{ browser: 'chromium', setupFiles: './src/test/setup.ts' }],
     },
   },
 });
